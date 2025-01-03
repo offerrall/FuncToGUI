97bc2e58890bb183b09fb05b848332f253438f15
offerrall <offerrallps4@gmail.com>
2025-01-03 16:45:42 +0100

    Add new test functions and output parameter inspection results to JSON files

--- aaa.py
+++ aaa.py
@@ -19,3 +19,20 @@ params = inspect_params(test)
 
 with open('params.json', 'w') as f:
     json.dump(params, f, indent=4)
+
+def test2() -> str:
+    test_str = strUi("hola", 0, 300)
+    return test_str
+
+params2 = inspect_params(test2)
+
+with open('params2.json', 'w') as f:
+    json.dump(params2, f, indent=4)
+
+def test3(hola) -> str:
+    return hola
+
+params3 = inspect_params(test3)
+
+with open('params3.json', 'w') as f:
+    json.dump(params3, f, indent=4)
