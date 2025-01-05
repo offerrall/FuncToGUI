2b6e96ca0231c932a4c0cbc895b987e84fcf791d
offerrall <offerrallps4@gmail.com>
2025-01-05 23:42:42 +0100

    Refactor styles.kv to adjust label heights and add padding; streamline canvas drawing for layout consistency in properties and result sections

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
 from functogui.ui_types import intUi
 from functogui import App
 
-num_leds = intUi(value=30, min_value=1, max_value=1000)
-brightness = intUi(value=100, min_value=0, max_value=100)
 
-def calculate_power(num_leds: int = num_leds,
-                    brightness: int = brightness
+def calculate_power(num_leds: int = intUi(value=30, min_value=1, max_value=1000),
+                    brightness: int = intUi(value=100, min_value=0, max_value=100)
                     ) -> float:
-
-    MAX_LED_POWER = 0.06
-    consumption = (MAX_LED_POWER * num_leds * (brightness/100))
+    consumption = (0.06 * num_leds * (brightness/100))
     return round(consumption, 2)
 
 app = App(calculate_power)
