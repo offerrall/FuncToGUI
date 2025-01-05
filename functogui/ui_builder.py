83df9b1b5ef97874d73b3275808973cdde7c21d2
offerrall <offerrallps4@gmail.com>
2025-01-05 20:50:16 +0100

    Refactor MainLayout to improve UI structure and add dynamic title; enhance styles and spacing in styles.kv

--- functogui/ui_builder.py
+++ functogui/ui_builder.py
@@ -2,6 +2,8 @@ from kivy.app import App as KivyApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.metrics import dp
 from pathlib import Path
 
 from .ui_types import inspect_params
@@ -9,16 +11,19 @@ from .properties import *
 
 CURRENT_DIR = Path(__file__).parent.absolute()
 KV_FILE = CURRENT_DIR / "styles.kv"
-
+WINDOW_WIDTH = dp(400)
 Builder.load_file(str(KV_FILE))
 
 class MainLayout(BoxLayout):
+    title = StringProperty("Function GUI")
     
     def __init__(self, function, **kwargs):
         super().__init__(**kwargs)
         self.function = function
 
         properties = inspect_params(function)
+        name_function = function.__name__
+        self.title = name_function.replace("_", " ").title()
 
         for prop in properties:
             type_propertie = properties[prop]["type"]
@@ -26,7 +31,7 @@ class MainLayout(BoxLayout):
             default_params = properties[prop]["default_params"]
 
             if type_propertie == "int":
-                values = {"name": prop}
+                values = {"name": prop.replace("_", " ").title()}
                 
                 for key, value in default_params.items():
                     values[key] = value["default"]
@@ -34,17 +39,19 @@ class MainLayout(BoxLayout):
                 for key, value in constructor_values.items():
                     values[key] = value
 
-                self.add_widget(CustomIntProperty(**values))
+                self.ids.properties_layout.add_widget(CustomIntProperty(**values))
 
         empty_widget = Widget()
-        self.add_widget(empty_widget)
-    
+        self.ids.properties_layout.add_widget(empty_widget)
 
 class App(KivyApp):
-
     def __init__(self, function, **kwargs):
         super().__init__(**kwargs)
         self.function = function
+
+        Window.size = (WINDOW_WIDTH, Window.size[1])
+        Window.minimum_width = WINDOW_WIDTH
+        Window.maximum_width = WINDOW_WIDTH
 
     def build(self):
         return MainLayout(self.function)
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 from functogui import App
 from functogui.ui_types import intUi
 
-def multiply(a: int = intUi(0),
-             b: int = intUi(0)
-             ) -> int:
-    return a * b
 
 
-app = App(multiply)
+num_leds = intUi(value=30, min_value=1, max_value=10000)
+brightness = intUi(value=100, min_value=0, max_value=100)
+
+def calculate_power(num_leds: int = num_leds,
+                    brightness: int = brightness
+                    ) -> float:
+
+    MAX_LED_POWER = 0.06
+    consumption = (MAX_LED_POWER * num_leds * (brightness/100))
+    return round(consumption, 2)
+
+app = App(calculate_power)
 app.run()
