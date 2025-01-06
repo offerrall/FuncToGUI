69922ad60d315d9af5242839707bc78a2e2360fd
offerrall <offerrallps4@gmail.com>
2025-01-06 23:22:16 +0100

    Refactor bool_return.py and str_return.py examples: adjust function signatures to improve parameter handling and enforce input constraints

--- examples/bool_return.py
+++ examples/bool_return.py
@@ -1,9 +1,8 @@
 from functogui import App
 from functogui.ui_types import intUi, boolUi, boolReturn
 
-def check_even_number(
-    number: int = intUi(value=4, min_value=0, max_value=100),
-    ) -> boolReturn:
+def check_even_number(number: int = intUi(value=4, min_value=0, max_value=100),
+                      ) -> boolReturn:
     
     return number % 2 == 0
 
