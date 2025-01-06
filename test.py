ed31c6b25a81da1ea705e4e7d382ecfaa43be34d
offerrall <offerrallps4@gmail.com>
2025-01-06 03:39:32 +0100

    Refactor styles and layout in MainLayout; adjust spacing and alignment for improved UI presentation

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from functogui.ui_types import intUi, strUi, boolUi, listUi, fileUi, inspect_params
 from functogui import App
 
+
+
 def concatenate_n_texts(text: str = strUi(value="Hello", min_length=2, max_length=5),
                         n: int = intUi(value=3, min_value=1, max_value=10),
                         add_exclamation_mark: bool = boolUi(),
