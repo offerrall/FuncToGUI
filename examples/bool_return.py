2d8ec3fe76657275ce5c660551f4482b041fe6e4
offerrall <offerrallps4@gmail.com>
2025-01-07 08:45:30 +0100

    consistency

--- examples/bool_return.py
+++ examples/bool_return.py
@@ -1,9 +1,6 @@
 from functogui import App, intUi, boolUi, boolReturn
 
-
-def check_even_number(number: int = intUi(4, max_value=100),
-                      ) -> boolReturn:
-    
+def check_even_number(number: int = intUi(4, max_value=100)) -> boolReturn:
     return number % 2 == 0
 
 App(check_even_number)
