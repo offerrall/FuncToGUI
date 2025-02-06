83f4577e2438a30187b32cacad859496b40be550
Beltrán Offerrall <69093559+offerrall@users.noreply.github.com>
2025-02-06 03:57:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="functogui",
-    version="0.0.2",
+    version="0.0.3",
     author="Beltrán Offerrall",
     packages=find_packages(),
     package_data={
