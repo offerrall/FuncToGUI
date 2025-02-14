d4c843e7ea01dc69bd2def86eec1e9dd90503ef3
Beltrán Offerrall <69093559+offerrall@users.noreply.github.com>
2025-02-14 18:31:46 +0100

    Replace quick.py with bool_example.py; add new example demonstrating boolean input handling

---
+++ examples/bool_example.py
@@ -0,0 +1,6 @@
+from functogui import App
+
+def no(input: bool = True) -> bool:
+    return not input
+
+App(no)
--- examples/quick.py
+++
@@ -1,6 +0,0 @@
-from functogui import App
-
-def check_even_number(number: int = 4) -> bool:
-    return number % 2 == 0
-
-App(check_even_number)
